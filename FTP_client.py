a17b5daad7a85ef9ed5d45289dc63742d33264c7
TheresaCA <113627002+TheresaCA@users.noreply.github.com>
2025-06-02 10:52:08 +0530

    FTP_client.py

--- FTP_client.py
+++ FTP_client.py
@@ -9,8 +9,13 @@ SIZE = 1024
 
 def main():
     """ Starting a TCP socket with SSL/TLS support. """
+    context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
+    context.load_cert_chain(certfile="client.crt", keyfile="client.key")  # Client's cert and key
+    context.load_verify_locations(cafile="server.crt")  # Used to verify the server's cert
+
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    ssl_client = ssl.wrap_socket(client, ssl_version=ssl.PROTOCOL_TLS)
+    ssl_client = context.wrap_socket(client, server_hostname=IP)
+
 
     """ Connecting to the server using SSL/TLS. """
     ssl_client.connect(ADDR)
@@ -35,5 +40,5 @@ def main():
     """ Closing the connection from the server. """
     ssl_client.close()
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     main()
